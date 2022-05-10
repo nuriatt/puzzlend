25c93d8f52ce29a72894f63b8f5b0a6fb816ac20
Nuria Toledano <ia.andyouare@gmail.com>
2022-05-10 20:24:47 +0100

    Added views and controller actions for puzzles

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,15 @@ ActiveRecord::Schema.define(version: 2022_05_07_154615) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "puzzle_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["puzzle_id"], name: "index_bookings_on_puzzle_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
   create_table "puzzles", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -35,4 +44,6 @@ ActiveRecord::Schema.define(version: 2022_05_07_154615) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "puzzles"
+  add_foreign_key "bookings", "users"
 end
