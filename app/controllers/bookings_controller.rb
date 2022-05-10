7f050f8da2ea99e1fb4d6e57590c4c19a296b236
Tom Brookes <tom.brookes15@gmail.com>
2022-05-10 21:31:16 +0100

    added methods to bookings controller, not tested, awaiting seed file

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,26 @@
 class BookingsController < ApplicationController
+  before_action :set_puzzle, only: [:new, :create]
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.puzzle = @puzzle
+    if @booking.save
+      redirect_to puzzle_path(@ouzzle)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:puzzle_id)
+  end
+
+  def set_puzzle
+    @puzzle = Puzzle.find(:puzzle_id)
+  end
 end
