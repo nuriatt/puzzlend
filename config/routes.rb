d401b821e94b35237ead05ebc8a964b4e95f085a
Tom Brookes <tom.brookes15@gmail.com>
2022-05-19 19:31:42 +0100

    working on the update button

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'my_puzzles', action: :my_puzzles, controller: 'users'
   get 'my_bookings', action: :my_bookings, controller: 'users'
 
-  resources :bookings, only: [:index, :destroy]
+  resources :bookings, only: [:index, :destroy, :edit, :update]
 
   resources :puzzles do
     resources :bookings, only: [:new, :create]
